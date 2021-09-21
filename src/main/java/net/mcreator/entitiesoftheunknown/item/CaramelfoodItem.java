480a79b06b241b4c3a4798f8fb25406f8cf00bb2
the-soul-powder <tianna.kahwaji@gmail.com>
2021-09-20 23:50:30 -0500

    sync #8

---
+++ src/main/java/net/mcreator/entitiesoftheunknown/item/CaramelfoodItem.java
@@ -0,0 +1,59 @@
+
+package net.mcreator.entitiesoftheunknown.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.world.World;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.item.Rarity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.block.BlockState;
+
+import net.mcreator.entitiesoftheunknown.EntitiesOfTheUnknownModElements;
+
+import java.util.List;
+
+@EntitiesOfTheUnknownModElements.ModElement.Tag
+public class CaramelfoodItem extends EntitiesOfTheUnknownModElements.ModElement {
+	@ObjectHolder("entities_of_the_unknown:caramelfood")
+	public static final Item block = null;
+	public CaramelfoodItem(EntitiesOfTheUnknownModElements instance) {
+		super(instance, 16);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			super(new Item.Properties().group(ItemGroup.MISC).maxStackSize(64).rarity(Rarity.COMMON));
+			setRegistryName("caramelfood");
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public int getUseDuration(ItemStack itemstack) {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
+			return 1F;
+		}
+
+		@Override
+		public void addInformation(ItemStack itemstack, World world, List<ITextComponent> list, ITooltipFlag flag) {
+			super.addInformation(itemstack, world, list, flag);
+			list.add(new StringTextComponent("so tasty but so sticky once its on your clothes"));
+		}
+	}
+}
