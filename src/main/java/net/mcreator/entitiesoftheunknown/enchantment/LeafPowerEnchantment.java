d17d8924dd4cdedbe9b5bc5247047de20694a6ee
hypercreeper <hyper1creeper@gmail.com>
2021-10-11 11:22:41 +0400

    Sync #31
    - Fixed Leaf power stats

---
+++ src/main/java/net/mcreator/entitiesoftheunknown/enchantment/LeafPowerEnchantment.java
@@ -0,0 +1,88 @@
+
+package net.mcreator.entitiesoftheunknown.enchantment;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.util.DamageSource;
+import net.minecraft.item.ItemStack;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.enchantment.EnchantmentType;
+import net.minecraft.enchantment.Enchantment;
+
+import net.mcreator.entitiesoftheunknown.item.LeafswordItem;
+import net.mcreator.entitiesoftheunknown.item.LeafbulletItem;
+import net.mcreator.entitiesoftheunknown.item.LeafbowItem;
+import net.mcreator.entitiesoftheunknown.item.LeafAxeItem;
+import net.mcreator.entitiesoftheunknown.EntitiesOfTheUnknownModElements;
+
+@EntitiesOfTheUnknownModElements.ModElement.Tag
+public class LeafPowerEnchantment extends EntitiesOfTheUnknownModElements.ModElement {
+	@ObjectHolder("entities_of_the_unknown:leaf_power")
+	public static final Enchantment enchantment = null;
+	public LeafPowerEnchantment(EntitiesOfTheUnknownModElements instance) {
+		super(instance, 59);
+	}
+
+	@Override
+	public void initElements() {
+		elements.enchantments.add(() -> new CustomEnchantment(EquipmentSlotType.MAINHAND).setRegistryName("leaf_power"));
+	}
+	public static class CustomEnchantment extends Enchantment {
+		public CustomEnchantment(EquipmentSlotType... slots) {
+			super(Enchantment.Rarity.RARE, EnchantmentType.WEAPON, slots);
+		}
+
+		@Override
+		public int getMinLevel() {
+			return 1;
+		}
+
+		@Override
+		public int getMaxLevel() {
+			return 5;
+		}
+
+		@Override
+		public int calcModifierDamage(int level, DamageSource source) {
+			return level * 2;
+		}
+
+		@Override
+		public boolean canApplyAtEnchantingTable(ItemStack stack) {
+			if (stack.getItem() == LeafswordItem.block)
+				return true;
+			if (stack.getItem() == LeafbowItem.block)
+				return true;
+			if (stack.getItem() == LeafAxeItem.block)
+				return true;
+			if (stack.getItem() == LeafbulletItem.block)
+				return true;
+			return false;
+		}
+
+		@Override
+		public boolean isTreasureEnchantment() {
+			return true;
+		}
+
+		@Override
+		public boolean isCurse() {
+			return false;
+		}
+
+		@Override
+		public boolean isAllowedOnBooks() {
+			return true;
+		}
+
+		@Override
+		public boolean canGenerateInLoot() {
+			return true;
+		}
+
+		@Override
+		public boolean canVillagerTrade() {
+			return true;
+		}
+	}
+}
