e8bda82d5938815a35aabea20ee2e2bcc1befcbf
hypercreeper <hyper1creeper@gmail.com>
2021-10-14 11:52:15 +0400

    Sync #34

--- src/main/java/net/mcreator/entitiesoftheunknown/entity/VlexadoiteEntity.java
+++ src/main/java/net/mcreator/entitiesoftheunknown/entity/VlexadoiteEntity.java
@@ -2,6 +2,11 @@
 package net.mcreator.entitiesoftheunknown.entity;
 
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.items.wrapper.EntityHandsInvWrapper;
+import net.minecraftforge.items.wrapper.EntityArmorInvWrapper;
+import net.minecraftforge.items.wrapper.CombinedInvWrapper;
+import net.minecraftforge.items.ItemStackHandler;
+import net.minecraftforge.items.CapabilityItemHandler;
 import net.minecraftforge.fml.network.NetworkHooks;
 import net.minecraftforge.fml.network.FMLPlayMessages;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -9,21 +14,37 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.event.world.BiomeLoadingEvent;
 import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
+import net.minecraftforge.common.util.LazyOptional;
+import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.DungeonHooks;
 
 import net.minecraft.world.gen.Heightmap;
 import net.minecraft.world.biome.MobSpawnInfo;
 import net.minecraft.world.World;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.Hand;
+import net.minecraft.util.Direction;
 import net.minecraft.util.DamageSource;
+import net.minecraft.util.ActionResultType;
+import net.minecraft.network.PacketBuffer;
 import net.minecraft.network.IPacket;
+import net.minecraft.nbt.INBT;
+import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.item.SpawnEggItem;
 import net.minecraft.item.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.Item;
+import net.minecraft.inventory.container.INamedContainerProvider;
+import net.minecraft.inventory.container.Container;
 import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.ai.goal.SwimGoal;
 import net.minecraft.entity.ai.goal.RangedAttackGoal;
 import net.minecraft.entity.ai.goal.RandomWalkingGoal;
@@ -39,11 +60,18 @@ import net.minecraft.entity.EntitySpawnPlacementRegistry;
 import net.minecraft.entity.EntityClassification;
 import net.minecraft.entity.CreatureEntity;
 import net.minecraft.entity.CreatureAttribute;
+import net.minecraft.enchantment.EnchantmentHelper;
 
 import net.mcreator.entitiesoftheunknown.item.LeafswordItem;
 import net.mcreator.entitiesoftheunknown.item.LeafbowItem;
+import net.mcreator.entitiesoftheunknown.gui.VlexadoiteTradeGUIGui;
 import net.mcreator.entitiesoftheunknown.entity.renderer.VlexadoiteRenderer;
 import net.mcreator.entitiesoftheunknown.EntitiesOfTheUnknownModElements;
+
+import javax.annotation.Nullable;
+import javax.annotation.Nonnull;
+
+import io.netty.buffer.Unpooled;
 
 @EntitiesOfTheUnknownModElements.ModElement.Tag
 public class VlexadoiteEntity extends EntitiesOfTheUnknownModElements.ModElement {
@@ -101,6 +129,7 @@ public class VlexadoiteEntity extends EntitiesOfTheUnknownModElements.ModElement
 			super(type, world);
 			experienceValue = 5;
 			setNoAI(false);
+			enablePersistence();
 			this.setItemStackToSlot(EquipmentSlotType.MAINHAND, new ItemStack(LeafswordItem.block));
 		}
 
@@ -129,6 +158,11 @@ public class VlexadoiteEntity extends EntitiesOfTheUnknownModElements.ModElement
 			return CreatureAttribute.ARTHROPOD;
 		}
 
+		@Override
+		public boolean canDespawn(double distanceToClosestPlayer) {
+			return false;
+		}
+
 		protected void dropSpecialItems(DamageSource source, int looting, boolean recentlyHitIn) {
 			super.dropSpecialItems(source, looting, recentlyHitIn);
 			this.entityDropItem(new ItemStack(Items.GOLD_INGOT));
@@ -142,6 +176,74 @@ public class VlexadoiteEntity extends EntitiesOfTheUnknownModElements.ModElement
 		@Override
 		public net.minecraft.util.SoundEvent getDeathSound() {
 			return (net.minecraft.util.SoundEvent) ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("entity.generic.death"));
+		}
+		private final ItemStackHandler inventory = new ItemStackHandler(9) {
+			@Override
+			public int getSlotLimit(int slot) {
+				return 64;
+			}
+		};
+		private final CombinedInvWrapper combined = new CombinedInvWrapper(inventory, new EntityHandsInvWrapper(this),
+				new EntityArmorInvWrapper(this));
+		@Override
+		public <T> LazyOptional<T> getCapability(@Nonnull Capability<T> capability, @Nullable Direction side) {
+			if (this.isAlive() && capability == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY && side == null)
+				return LazyOptional.of(() -> combined).cast();
+			return super.getCapability(capability, side);
+		}
+
+		@Override
+		protected void dropInventory() {
+			super.dropInventory();
+			for (int i = 0; i < inventory.getSlots(); ++i) {
+				ItemStack itemstack = inventory.getStackInSlot(i);
+				if (!itemstack.isEmpty() && !EnchantmentHelper.hasVanishingCurse(itemstack)) {
+					this.entityDropItem(itemstack);
+				}
+			}
+		}
+
+		@Override
+		public void writeAdditional(CompoundNBT compound) {
+			super.writeAdditional(compound);
+			compound.put("InventoryCustom", inventory.serializeNBT());
+		}
+
+		@Override
+		public void readAdditional(CompoundNBT compound) {
+			super.readAdditional(compound);
+			INBT inventoryCustom = compound.get("InventoryCustom");
+			if (inventoryCustom instanceof CompoundNBT)
+				inventory.deserializeNBT((CompoundNBT) inventoryCustom);
+		}
+
+		@Override
+		public ActionResultType func_230254_b_(PlayerEntity sourceentity, Hand hand) {
+			ItemStack itemstack = sourceentity.getHeldItem(hand);
+			ActionResultType retval = ActionResultType.func_233537_a_(this.world.isRemote());
+			if (sourceentity instanceof ServerPlayerEntity) {
+				NetworkHooks.openGui((ServerPlayerEntity) sourceentity, new INamedContainerProvider() {
+					@Override
+					public ITextComponent getDisplayName() {
+						return new StringTextComponent("Vlexadoite");
+					}
+
+					@Override
+					public Container createMenu(int id, PlayerInventory inventory, PlayerEntity player) {
+						PacketBuffer packetBuffer = new PacketBuffer(Unpooled.buffer());
+						packetBuffer.writeBlockPos(new BlockPos(sourceentity.getPosition()));
+						packetBuffer.writeByte(0);
+						packetBuffer.writeVarInt(CustomEntity.this.getEntityId());
+						return new VlexadoiteTradeGUIGui.GuiContainerMod(id, inventory, packetBuffer);
+					}
+				}, buf -> {
+					buf.writeBlockPos(new BlockPos(sourceentity.getPosition()));
+					buf.writeByte(0);
+					buf.writeVarInt(this.getEntityId());
+				});
+			}
+			super.func_230254_b_(sourceentity, hand);
+			return retval;
 		}
 
 		public void attackEntityWithRangedAttack(LivingEntity target, float flval) {
---
+++ src/main/java/net/mcreator/entitiesoftheunknown/gui/VlexadoiteTradeGUIGuiWindow.java
@@ -0,0 +1,92 @@
+
+package net.mcreator.entitiesoftheunknown.gui;
+
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.world.World;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.client.gui.screen.inventory.ContainerScreen;
+import net.minecraft.client.Minecraft;
+
+import java.util.HashMap;
+
+import com.mojang.blaze3d.systems.RenderSystem;
+import com.mojang.blaze3d.matrix.MatrixStack;
+
+@OnlyIn(Dist.CLIENT)
+public class VlexadoiteTradeGUIGuiWindow extends ContainerScreen<VlexadoiteTradeGUIGui.GuiContainerMod> {
+	private World world;
+	private int x, y, z;
+	private PlayerEntity entity;
+	private final static HashMap guistate = VlexadoiteTradeGUIGui.guistate;
+	public VlexadoiteTradeGUIGuiWindow(VlexadoiteTradeGUIGui.GuiContainerMod container, PlayerInventory inventory, ITextComponent text) {
+		super(container, inventory, text);
+		this.world = container.world;
+		this.x = container.x;
+		this.y = container.y;
+		this.z = container.z;
+		this.entity = container.entity;
+		this.xSize = 200;
+		this.ySize = 300;
+	}
+	private static final ResourceLocation texture = new ResourceLocation("entities_of_the_unknown:textures/vlexadoite_trade_gui.png");
+	@Override
+	public void render(MatrixStack ms, int mouseX, int mouseY, float partialTicks) {
+		this.renderBackground(ms);
+		super.render(ms, mouseX, mouseY, partialTicks);
+		this.renderHoveredTooltip(ms, mouseX, mouseY);
+	}
+
+	@Override
+	protected void drawGuiContainerBackgroundLayer(MatrixStack ms, float partialTicks, int gx, int gy) {
+		RenderSystem.color4f(1, 1, 1, 1);
+		RenderSystem.enableBlend();
+		RenderSystem.defaultBlendFunc();
+		Minecraft.getInstance().getTextureManager().bindTexture(texture);
+		int k = (this.width - this.xSize) / 2;
+		int l = (this.height - this.ySize) / 2;
+		this.blit(ms, k, l, 0, 0, this.xSize, this.ySize, this.xSize, this.ySize);
+		RenderSystem.disableBlend();
+	}
+
+	@Override
+	public boolean keyPressed(int key, int b, int c) {
+		if (key == 256) {
+			this.minecraft.player.closeScreen();
+			return true;
+		}
+		return super.keyPressed(key, b, c);
+	}
+
+	@Override
+	public void tick() {
+		super.tick();
+	}
+
+	@Override
+	protected void drawGuiContainerForegroundLayer(MatrixStack ms, int mouseX, int mouseY) {
+		this.font.drawString(ms, "Trade", 84, 36, -12829636);
+		this.font.drawString(ms, "Gold", 16, 76, -12829636);
+		this.font.drawString(ms, "Leaf", 53, 76, -12829636);
+		this.font.drawString(ms, "Oak Log", 81, 75, -12829636);
+		this.font.drawString(ms, "Oak Leaves", 146, 76, -12829636);
+		this.font.drawString(ms, "Lapis", 119, 76, -12829636);
+		this.font.drawString(ms, "Output", 84, 150, -12829636);
+	}
+
+	@Override
+	public void onClose() {
+		super.onClose();
+		Minecraft.getInstance().keyboardListener.enableRepeatEvents(false);
+	}
+
+	@Override
+	public void init(Minecraft minecraft, int width, int height) {
+		super.init(minecraft, width, height);
+		minecraft.keyboardListener.enableRepeatEvents(true);
+	}
+}
