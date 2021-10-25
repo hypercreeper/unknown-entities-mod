c0bf5fb2d852ca059781adf0cc59299e0ee63bc4
hypercreeper <hyper1creeper@gmail.com>
2021-10-25 10:58:46 +0400

    Sync #39
    - Fixed discs appearance error
    - Updated special info for discs

--- src/main/java/net/mcreator/entitiesoftheunknown/item/TheSweetSoundOfAmbienceItem.java
+++ src/main/java/net/mcreator/entitiesoftheunknown/item/TheSweetSoundOfAmbienceItem.java
@@ -5,14 +5,20 @@ import net.minecraftforge.registries.ObjectHolder;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.api.distmarker.Dist;
 
+import net.minecraft.world.World;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.item.Rarity;
 import net.minecraft.item.MusicDiscItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Item;
+import net.minecraft.client.util.ITooltipFlag;
 
 import net.mcreator.entitiesoftheunknown.itemgroup.AncientItemsItemGroup;
 import net.mcreator.entitiesoftheunknown.EntitiesOfTheUnknownModElements;
+
+import java.util.List;
 
 @EntitiesOfTheUnknownModElements.ModElement.Tag
 public class TheSweetSoundOfAmbienceItem extends EntitiesOfTheUnknownModElements.ModElement {
@@ -37,6 +43,12 @@ public class TheSweetSoundOfAmbienceItem extends EntitiesOfTheUnknownModElements
 		@OnlyIn(Dist.CLIENT)
 		public boolean hasEffect(ItemStack itemstack) {
 			return true;
+		}
+
+		@Override
+		public void addInformation(ItemStack itemstack, World world, List<ITextComponent> list, ITooltipFlag flag) {
+			super.addInformation(itemstack, world, list, flag);
+			list.add(new StringTextComponent("im not sure you figure it out"));
 		}
 	}
 }
